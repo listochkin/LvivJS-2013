7ddbe21daaee650eca03f444c63dac5386db9dc0
Андрей Листочкин <andrew.listochkin@gmail.com>
2013-03-30 12:07:33 +0200

    Mocks

--- server/server.js
+++ server/server.js
@@ -15,28 +15,33 @@ var fs = require('fs'),
 app.route('/').file(path.join(__dirname, '../public/index.html'));
 app.route('/*').files(path.join(__dirname, '../public'));
 
+var useMocks = true;
 
-var sockets = [];
-var twitStream = require('./twitter');
+if (!useMocks) {
+    var sockets = [];
+    var twitStream = require('./twitter');
 
-twitStream.on('tweet', function (tweet) {
-    // console.log(tweet);
-    sockets.forEach(function (socket) {
-        socket.emit('tweet', stringify(tweet));
-    })
-});
+    twitStream.on('tweet', function (tweet) {
+        // console.log(tweet);
+        sockets.forEach(function (socket) {
+            socket.emit('tweet', stringify(tweet));
+        })
+    });
 
-twitStream.start();
-
-var twits = fs.readFileSync(path.join(__dirname, 'tweets.mock.json'), 'utf8').split('\n');
-var i = 0;
+    twitStream.start();    
+} else {
+    var tweets = fs.readFileSync(path.join(__dirname, 'tweets.mock.json'), 'utf8').split('\n');
+    var i = 0;
+}
 app.sockets.on('connection', function (socket) {
-    sockets.push(socket);
-	// setInterval(function () {
-	// 	var twit = twits[i];
-	// 	// console.log(twit);
-	// 	socket.emit('twit', twit);
-	// 	i = (i + 1) % twits.length;
-	// }, 1000);
+    if (!useMocks) {
+        sockets.push(socket);
+    } else {
+        setInterval(function () {
+            var tweet = tweets[i];
+            socket.emit('tweet', tweet);
+            i = (i + 1) % tweets.length;
+        }, 1000);
+    }
 });
 app.httpServer.listen(8008);
